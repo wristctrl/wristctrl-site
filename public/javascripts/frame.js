d68daa0120488fc88c32c9bfdd14bc1cf284e10d
Kirby Kohlmorgen <kirby@purdue.edu>
2015-01-22 15:26:07 -0500

    Fix #9

--- public/javascripts/frame.js
+++ public/javascripts/frame.js
@@ -3,22 +3,27 @@ var app = angular.module("app", ["firebase"]);
 app.controller("MarketplaceCtrl", function($scope, $firebase) {
   var ref = new Firebase("https://8tracks-pebble.firebaseio.com/marketplace");
   var sync = $firebase(ref);
-  $scope.apps = sync.$asArray();
+  $scope.plugins = sync.$asArray();
 
-  $scope.addApp = cats;
+  $scope.addPlugin = addPlugin;
 });
 
-var cats = function(app) {
-  var currApp = app.$id;
+var addPlugin = function(plugin) {
+  var currPlugin = plugin.$id;
 
   var uniqueId = localStorage.getItem("ctrl-uniqueId");
 
   var fb = new Firebase('https://8tracks-pebble.firebaseio.com/');
 
-  fb.child('marketplace').child(currApp).once('value', function(snapshot) {
-    var appCopy = snapshot.val();
-    fb.child('codes').child(uniqueId).child(currApp).update(appCopy);
-    fb.child('codes').child(uniqueId).child(currApp).child('img').remove();
+  fb.child('marketplace').child(currPlugin).once('value', function(snapshot) {
+    var pluginCopy = snapshot.val();
+
+    fb.child('codes').child(uniqueId).child('plugins').child(currPlugin).update(pluginCopy);
+    fb.child('codes').child(uniqueId).child('plugins').child(currPlugin).child('img').remove();
+
+    var pluginMap = {};
+    pluginMap[currPlugin] = pluginCopy.map.site;
+    fb.child('codes').child(uniqueId).child('plugin-map').update(pluginMap);
   });
 
 };
